e40dd4b3faba094134422a2fbe89ea1ea8d007f2
mmvergara <mark.jesusmanabat@gmail.com>
2023-09-24 13:17:00 +0800

    redis-connect

--- application/app.go
+++ application/app.go
@@ -4,16 +4,25 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"os"
+
 	"github.com/mmvergara/go-simple-api/routes"
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	redisDb *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: routes.LoadRoutes(),
+		redisDb: redis.NewClient(&redis.Options{
+			Addr: os.Getenv("REDIS_DB_URL"),
+			Username: os.Getenv("REDIS_USERNAME"),
+			Password: os.Getenv("REDIS_PASSWORD"),
+		}),
 	}
 
 	return app
@@ -24,8 +33,16 @@ func (a *App) Start(ctx context.Context) error {
 		Addr: ":3000",
 		Handler: a.router,
 	}
+	
+	fmt.Println("Connecting to redis")
+	err := a.redisDb.Ping(ctx).Err()
 
-	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to connect to redis %w",err)
+	}
+
+	fmt.Println("Startin Server")
+	err = server.ListenAndServe()
 
 	if err != nil{
 		return fmt.Errorf("failed to start server %w", err)
--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/joho/godotenv"
 	"github.com/mmvergara/go-simple-api/application"
 )
 
 func main(){
+	godotenv.Load(".env")
 	app := application.New()
 
 	err := app.Start(context.TODO())
-
 	if err != nil {
 		fmt.Println("Failed to start app:", err)
 	}
